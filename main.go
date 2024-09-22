ffcd3884ae2302900bc676ce8cd0021ed2328355
berzoc <berzoc@gmail.com>
2024-09-21 22:34:28 -0500

    grpc-timestampbb attribute added

--- main.go
+++ main.go
@@ -8,17 +8,38 @@ import (
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
+	"time"
 )
 
 type myReaderServer struct {
+	//reader.UnimplementedReaderServer
 	reader.UnimplementedReaderServer
 }
 
 func (s myReaderServer) Report(ctx context.Context, request *reader.ReportRequest) (*reader.ReportResponse, error) {
-	fmt.Printf("TagId: %d , EventID: %d, time: \n", request.TagId, request.EventId)
+	fmt.Printf("TagId: %d , EventID: %d, time:%s \n", request.TagId, request.EventId, request.RunnerTime.AsTime().Format("01-02-2006 15:04:05"))
 	return &reader.ReportResponse{
 		Status: true,
 	}, nil
+}
+
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Events Handler")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	//w.Header().Set("Access-Control-Expose-Headers", "Content-Type")
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	for i := 0; i < 20; i++ {
+		//fmt.Fprintf(w, "data: %d\n\n", i)
+		newd := fmt.Sprintf(`data: {"runner":{"tagId":%d,"eventId":%d}}`, i, i)
+		fmt.Fprintf(w, "%s\n\n", newd)
+		w.(http.Flusher).Flush()
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func main() {
@@ -36,4 +57,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("error during server registrar %s", err)
 	}
+
+	//http.HandleFunc("/runners", eventsHandler)
+	//http.ListenAndServe(":8090", nil)
+
 }
